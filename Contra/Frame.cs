eac66303271731a7bf66c391370548702e53fcc1
Zanlin Oo <zanlin87@gmail.com>
2020-10-07 10:32:53 -0700

    Add project files.

---
+++ Contra/Frame.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contra
+{
+    class Frame
+    {
+        public Rectangle CutoutFrame { get; set; }
+        public Vector2 RotationOrigin
+        {
+            get
+            {
+                return RotationOrigin;
+            }
+            set
+            {
+                float x = CutoutFrame.X + (int)OriginOffset.X;
+                float y = CutoutFrame.Y + (int)OriginOffset.Y;
+                new Vector2(x, y);
+            }
+        }
+        public Vector2 OriginOffset { get; set; }
+        public Frame(Rectangle CutoutFrame, Vector2 OriginOffset)
+        {
+            this.CutoutFrame = CutoutFrame;
+            this.OriginOffset = OriginOffset;
+        }
+    }
+}
